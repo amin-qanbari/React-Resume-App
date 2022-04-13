f70cf46cd060cc50967c0a6a621a41911ebb8b49
amin <aminqanbarii1992@gmail.com>
2022-04-13 04:07:42 -0700

    create profile i font awesome and import to the app

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='profile-container'>
+        <div className="profile-parent">
+            <div className="profile-details">
+                <div className="colz">
+                    <a href="https://t.me/amin_qanbarii">
+                        <i className='fa fa-telegram'></i>
+                    </a>
+                    <a href="mailto:aminqanbarii1992@gmail.com">
+                        <i className='fa fa-envelope-square'></i>
+                    </a>
+                    <a href="https://www.instagram.com/amin_qanbarii">
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href="https://www.github.com/amin-qanbari">
+                        <i className='fa fa-github'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
